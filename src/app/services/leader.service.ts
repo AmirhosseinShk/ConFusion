412ca13db2c96ac6fff50961262582eab5b64d5d
Amirhossein shariatkia <amirshk76@outlook.com>
2017-10-04 18:41:44 +0330

    promise part 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -9,11 +9,24 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve => {
+      //Sumilate server latency with 2 second delay
+      setTimeout(() => resolve(LEADERS) , 2000);
+    });
   }
 
   getLeader(id: number): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve => {
+      //Sumilate server latency with 2 second delay
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]) , 2000);
+    });
+  }
+
+  getFeaturedLeader() : Promise<Leader> {
+    return new Promise(resolve => {
+      //Sumilate server latency with 2 second delay
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.featured))[0]) , 2000);
+    });
   }
 
 }
