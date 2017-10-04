9f16a79dac0497127f48663299c5f7e9d02786aa
Amirhossein shariatkia <amirshk76@outlook.com>
2017-10-04 17:09:22 +0330

    reactive forms part one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 import 'hammerjs';
 import { AppComponent } from './app.component';
@@ -40,7 +41,8 @@ import { LoginComponent } from './login/login.component';
     MaterialModule,
     FlexLayoutModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ DishService , PromotionService , LeaderService],
   entryComponents: [ LoginComponent ],
